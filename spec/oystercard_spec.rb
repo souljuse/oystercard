2fe065e5f52c27d001da65032f6abe7fa1267380
Amaal Ali <amq.ali@gmail.com>
2016-10-03 15:05:49 +0100

    Unit test for top-up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+  
+  describe '#top_up' do
+	  it { is_expected.to respond_to(:to_up) }
+  end
 end
