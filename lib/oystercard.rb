cdfc297e7c947b74b1ae5c492118b6a17722410f
Tom Nunn <t@tomnunn.uk>
2016-10-04 17:15:14 +0100

    create touch_in, touch_out and in_journey? methods with the related Examples

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,14 +4,28 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-    fail "Maximum balance exceeded. Maximum balance allowed is #{MAXIMUM_BALANCE}" if amount > (MAXIMUM_BALANCE - balance)
+    fail "Maximum balance exceeded. Maximum balance allowed is £#{MAXIMUM_BALANCE}" if amount > (MAXIMUM_BALANCE - balance)
     @balance += amount
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    fail "Insufficent balance to touch in" if balance < 1
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,16 +6,20 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-	  it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'can top up the balance' do
       expect { subject.top_up(10) }.to change{ subject.balance }.by 10
     end
 
-    it 'should raise error if maximum balance is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect { subject.top_up(1) }.to raise_error "Maximum balance exceeded. Maximum balance allowed is #{maximum_balance}"
+    context 'when topping up' do
+      before do
+        subject.top_up(Oystercard::MAXIMUM_BALANCE)
+      end
+
+  	  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+      it 'should raise error if maximum balance is exceeded' do
+        expect { subject.top_up(1) }.to raise_error "Maximum balance exceeded. Maximum balance allowed is £#{Oystercard::MAXIMUM_BALANCE}"
+      end
     end
   end
 
@@ -25,6 +29,36 @@ describe Oystercard do
     it 'should deduct amount from balance' do
       subject.top_up(10)
       expect { subject.deduct(5) }.to change{ subject.balance }.by -5
+    end
+  end
+
+  describe '#in_journey?' do
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  describe '#touch_in' do
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'can touch in' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'will not touch in if below minimum balance' do
+      # allow(subject).to receive(:balance) { 0 }
+      expect { subject.touch_in }.to raise_error "Insufficent balance to touch in"
+    end
+  end
+
+  describe '#touch_out' do
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'can touch out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
     end
   end
 end
