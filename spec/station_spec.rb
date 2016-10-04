acbf1e8dc791032d751fba29c80e21169818d634
Tomy8s <tomy8s@hotmail.com>
2016-10-04 13:50:59 +0100

    created station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,10 +1,11 @@
 require 'station'
 
 describe Station do
+  subject { Station.new("Central", 1) }
   it 'takes a name as an argument on initialization' do
-    expect(subject.name).to eq name
+    expect(subject.name).to eq "Central"
   end
   it 'takes a zone as an argument on initialization' do
-    expect(subject.zone).to eq zone
+    expect(subject.zone).to eq 1
   end
 end
