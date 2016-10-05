54be8cc4cfd0affa12a8dba489ca2ef5c49460ea
souljuse <oppo.irene@gmail.com>
2016-10-05 16:46:49 +0200

    step 14: extract class to the journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,26 @@
 class Journey
 
+  MINIMUM_BALANCE = 1
+  PENALTY_FARE = 6
+
+  attr_reader :entry_station, :fare
+
+  def initialize(args = {})
+    @entry_station = args[:entry_station]
+    @complete = false
+    @fare = PENALTY_FARE
+  end
+      # @journeys << { in: @entry_station, out: @exit_station }
+      # @entry_station = nil
+
+  def finish(station)
+    @complete = true
+    @fare = MINIMUM_BALANCE
+    self
+  end
+
+  def complete?
+    @complete
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,30 +1,43 @@
 require 'journey'
-require 'oystercard_spec'
-
 describe Journey do
-  let(:journey){ { in:entry_station, out:exit_station } }
+  let(:station) { double :station, zone: 1}
 
-  describe '#in_journey?' do
-    it 'is initially not in a journey' do
-      expect(subject).not_to be_in_journey
+  it "knows if a journey is not complete" do
+    expect(subject).not_to be_complete
+  end
+
+  it 'has a penalty fare by default' do
+    expect(subject.fare).to eq Journey::PENALTY_FARE
+  end
+
+  it "returns itself when exiting a journey" do
+    expect(subject.finish(station)).to eq(subject)
+  end
+
+  context 'given an entry station' do
+    subject {described_class.new(entry_station: station)}
+
+    it 'has an entry station' do
+      expect(subject.entry_station).to eq station
     end
 
-    it 'has an empty list of journeys by default' do
-      expect(subject.journeys).to be_empty
+    it "returns a penalty fare if no exit station given" do
+      expect(subject.fare).to eq Journey::PENALTY_FARE
     end
 
-    context 'when touching in' do
-      it 'stores the entry station' do
-        subject.touch_in(entry_station)
-        expect(subject.entry_station).to eq entry_station
+    context 'given an exit station' do
+      let(:other_station) { double :other_station }
+
+      before do
+        subject.finish(other_station)
       end
-    end
 
-    context 'when touching out' do
-      it 'stores exit station' do
-        subject.touch_in(entry_station)
-        subject.touch_out(exit_station)
-        expect(subject.journeys).to include journey
+      it 'calculates a fare' do
+        expect(subject.fare).to eq 1
+      end
+
+      it "knows if a journey is complete" do
+        expect(subject).to be_complete
       end
     end
   end
