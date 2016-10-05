a491f3b2bd249d8b9d7d1ffe68ce03588dc3c49a
Tom Nunn <t@tomnunn.uk>
2016-10-05 12:01:49 +0100

    add station class and initialize method with one argument

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,16 +1,17 @@
 require 'station'
 
 describe Station do
+  subject { described_class.new(name: "Old Street", zone: 1) }
 
   describe "#name" do
     it "should know the station" do
-      expect(subject.name).to eq name
+      expect(subject.name).to eq("Old Street")
     end
   end
 
   describe "#zone" do
     it "should know the zone" do
-      expect(subject.zone).to eq zone
+      expect(subject.zone).to eq(1)
     end
   end
 
