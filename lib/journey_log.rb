ba618c9b1116f38acfc32cddba30ac3ad3eb27e5
crispinandrews <crispin.andrews@gmail.com>
2016-10-06 17:00:18 +0100

    Stage 15 complete

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -2,18 +2,27 @@ require_relative 'journey'
 
 class Journeylog
 
-  attr_reader :current_journey
-
-  def initialize
+  def initialize(journey_class: Journey)
+    @journey_class = journey_class
     @journeys = []
     @current_journey = []
   end
 
   def start(station)
-    Journey.new(entry_station: station)
     @current_journey << { entry_station: station }
   end
 
+  def finish(station)
+    @current_journey << {exit_station: station}
+    @journeys << @current_journey
+    @current_journey = []
+  end
+
+  def journeys
+    @journeys.dup
+  end
+
+private
   def current_journey
     @current_journey || self.start(station)
   end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,33 +2,33 @@ require 'journey_log'
 
 describe Journeylog do
 
-  let(:journey) { double :journey }
+  let(:journey) { double :journey, entry_station: "Central" }
   let(:station) { double :station, name: "Central", zone: 1}
+  subject { described_class.new }
 
-  describe "#start" do
-
-    it 'should start a new journey with an entry station' do
-      allow(journey).to receive(:station)
-      expect(subject.start(station)).to eq journey(entry_station: "Central")
-    end
-  end
-
-  describe "#current_journey" do
-    it 'should return an incomplete journey' do
-      subject.start("Central")
-      expect(subject.current_journey).to include({entry_station: "Central"})
-    end
-    it 'should create a new journey' do
-      expect(subject.current_journey).to be_empty
-      subject.start("Central")
-    end
-  end
+  # describe "#current_journey" do
+  #   it 'should return an incomplete journey' do
+  #     subject.start("Central")
+  #     expect(subject.current_journey).to include({entry_station: "Central"})
+  #   end
+  #
+  #   it 'should create a new journey' do
+  #     expect(subject.current_journey).to be_empty
+  #     subject.start("Central")
+  #   end
+  # end
 
   describe "#finish" do
-    it 'should add an exit station to the current_journey' do
+    it 'should add current journey to journeys' do
+      subject.start("Central")
       subject.finish("South")
-      expect(subject.current_journey).to include({exit_station: "South"})
+      expect(subject.journeys).to include([{entry_station: "Central"}, {exit_station: "South"}])
     end
   end
+# 
+#   it 'should add an exit station to the current_journey' do
+#   subject.start("South")
+#   expect(subject.current_journey).to include({entry_station: "South"})
+# end
 
 end
