cdf32e4b006136ae5268065ce56e22bd3ec6dd08
crispinandrews <crispin.andrews@gmail.com>
2016-10-06 12:02:41 +0100

    Challenge 15 creating current journey method. Tests failing

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,7 +1,16 @@
+require_relative 'journey'
+
 class Journeylog
 
   def initialize
 
+  end
+
+  def start(station)
+    Journey.new(entry_station: station)
+  end
+
+  def current_journey
   end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,15 +2,25 @@ require 'journey_log'
 
 describe Journeylog do
 
-  let(:journey) { double :journey, entry_station: "Central" }
+  let(:journey) { double :journey }
   let(:station) { double :station, zone: 1}
 
   describe "#start" do
 
     it 'should start a new journey with an entry station' do
       allow(journey).to receive(:station)
-      expect(subject.start(station)).to eq(journey(entry_station: station))
+      expect(subject.start(station)).to eq journey(entry_station: "Central")
     end
-  end  
+  end
+
+  describe "#current_journey" do
+    it 'should return an incomplete journey' do
+      expect(subject.current_journey).to eq true
+    end
+    it 'should create a new journey' do
+      allow(subject).to receive(current_journey).and_return(false)
+      expect(subject.current_journey).to subject.start(station)
+    end
+  end
 
 end
